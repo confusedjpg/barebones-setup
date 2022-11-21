dd130373ffa96523bb670ff607813946956d91e3
confused.jpg <64487613+confusedjpg@users.noreply.github.com>
2022-11-21 11:33:32 +0100

    start services after enabling

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ pacman -S iwd xbacklight dhcpcd alsa-utils amixer openssh
 mv startup.sh /usr/local/sbin/
 mv startup.service /etc/systemd/system
 
-systemctl enable startup.service iwd dhcpcd sshd
+systemctl enable --now startup.service iwd dhcpcd sshd # '--now' also starts the service upon enabling
 
 dhcpcd wlan0 # enable dhcp on wlan0, this can sometimes require a different name depending on the device
 printf 'Pn1q7hNM34fpGcAVq9\n' | iwctl station wlan0 connect "Bbox-1DB9A449" psk #connect to network
